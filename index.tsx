19e27442ff9cfab82c29b6cb60c434c7492e749a
Brendan Forster <brendan@github.com>
2017-05-05 13:56:01 +1000

    naming things

--- index.tsx
+++ index.tsx
@@ -2,15 +2,11 @@ import * as React from 'react'
 
 import { Row } from './row'
 
-interface IAboutProps {
+interface IIndexProps {
 
 }
 
-/**
- * A dialog that presents information about the
- * running application such as name and version.
- */
-export class About extends React.Component<IAboutProps, void> {
+export class Index extends React.Component<IIndexProps, void> {
   public render() {
     return (
         <Row>
